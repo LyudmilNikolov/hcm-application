927b6e77212d400e81bb63e122bdc39cfa9f800c
Lyudmil Nikolov <nikolovlyudmill@gmail.com>
2024-03-22 20:54:47 +0200

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'ukg_front_end_assignment_2024_hcm_lyudmil_nikolov';
+}
